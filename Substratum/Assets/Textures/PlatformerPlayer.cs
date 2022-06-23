ac3406f7c6d34ff14336c74b6d1381054d57d914
Tonyy456 <ajdalesandro0115@gmail.com>
2022-06-22 20:04:45 -0400

    Added Player Controller

--- Substratum/Assets/Textures/PlatformerPlayer.cs
+++ Substratum/Assets/Textures/PlatformerPlayer.cs
@@ -2,21 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlatformerPlayer : MonoBehaviour
 {
     private float speed = 4.5f;
+    private float jumpForce = 5.0f;
+
 
     private Rigidbody2D body;
+    private BoxCollider2D box;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
+        box = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float deltaX = Input.GetAxis("Horizontal") * speed;
+        Vector2 movement = new Vector2(deltaX, body.velocity.y);
+        body.velocity = movement;
+
+        Vector3 max = box.bounds.max;
+        Vector3 min = box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = hit != null;
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
     }
 }
